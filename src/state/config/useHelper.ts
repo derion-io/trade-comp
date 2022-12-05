194258cf553cc0a3dbd2627c6ee8d0b9a4e8f2a0
khanhpham <khanhpham08121996@gmail.com>
2022-12-05 17:28:33 +0700

    24h change: update range *= 2 when do not have any log

--- src/state/config/useHelper.ts
+++ src/state/config/useHelper.ts
@@ -61,24 +61,30 @@ export const useHelper = () => {
       quoteToken,
       cToken,
       currentPrice,
-      baseId
+      baseId,
+      headBlock,
+      range = 40
     }: {
       baseToken: string,
       cToken: string,
       quoteToken: string,
       currentPrice: string,
-      baseId: number
-    }) => {
+      baseId: number,
+      headBlock?: number
+      range?: number
+    }): Promise<any> => {
     try {
       const provider = new JsonRpcProvider(configs.rpcToGetLogs)
-      const headBlock = await provider.getBlockNumber()
+      if (!headBlock) {
+        headBlock = await provider.getBlockNumber()
+      }
       const blocknumber24hAgo = headBlock - Math.floor(8640000 / configs.timePerBlock)
       const eventInterface = getEventInterface()
 
       const reserves = await provider.getLogs({
         address: cToken,
-        fromBlock: blocknumber24hAgo - 20,
-        toBlock: blocknumber24hAgo + 20,
+        fromBlock: blocknumber24hAgo - range,
+        toBlock: blocknumber24hAgo,
         topics: [
           '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1'
         ]
@@ -95,18 +101,30 @@ export const useHelper = () => {
         })
       })
 
-      const { baseReserve, quoteReserve } = reserves[0]
-      const price = weiToNumber(
-        quoteReserve.mul(numberToWei(1)).div(baseReserve),
-        18 + (tokens[quoteToken]?.decimal || 18) - (tokens[baseToken]?.decimal || 18)
-      )
-
-      return formatPercent(
-        div(
-          sub(currentPrice, price),
-          price
+      if (reserves.length > 0) {
+        const { baseReserve, quoteReserve } = reserves[reserves.length - 1]
+        const price = weiToNumber(
+          quoteReserve.mul(numberToWei(1)).div(baseReserve),
+          18 + (tokens[quoteToken]?.decimal || 18) - (tokens[baseToken]?.decimal || 18)
         )
-      )
+
+        return formatPercent(
+          div(
+            sub(currentPrice, price),
+            price
+          )
+        )
+      } else {
+        return await get24hChangeByLog({
+          baseToken,
+          quoteToken,
+          cToken,
+          currentPrice,
+          baseId,
+          headBlock,
+          range: range * 2
+        })
+      }
     } catch (e) {
       console.error(e)
       return 0
