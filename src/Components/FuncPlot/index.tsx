0faf3189687231ee3c7b2ae7273a3e2728c65cc8
Zergity <zergity@gmail.com>
2023-07-07 13:42:35 +0700

    remove the mobile GraphingCalculator

--- src/Components/FuncPlot/index.tsx
+++ src/Components/FuncPlot/index.tsx
@@ -57,251 +57,135 @@ export const FunctionPlot = (props: any) => {
   return (
     <React.Fragment>
       <Card className='p-1'>
-        {props.matches ? (
-          <div
-            className='flex flex-col justify-center items-center pt-[80px] pb-[80px] md:pb-[120px] md:pt-[120px] gap-6'>
-            <GraphingCalculator
-              attributes={{ className: 'calculator' }}
-              fontSize={18}
-              keypad
-              projectorMode
-              settingsMenu={false}
-              expressionsCollapsed
-              expressions={false}
-              zoomButtons={false}
-              lockViewport
-              invertedColors
-              border={false}
-              showGrid={false}
-              xAxisNumbers={false}
-              yAxisNumbers={false}
-              ref={calc}
-              xAxisArrowMode='POSITIVE'
-              yAxisArrowMode='POSITIVE'
-              xAxisLabel='Price'
-              yAxisLabel='Value'
-            >
-              <Expression id='f' latex={FX} hidden />
-              <Expression id='g' latex={GX} hidden />
-              <Expression
-                id='lR'
-                latex='(0.45,3.1)'
-                color='RED'
-                hidden
-                showLabel
-                label='Pool Reserve'
-                labelOrientation='RIGHT'
-              />
-              <Expression
-                id='R'
-                latex='y=3\{0.02<x\}'
-                color='RED'
-                lineWidth={1.5}
-              />
-              <Expression
-                id='rC'
-                latex='x=X\{f(X)<y<g(X)\}'
-                color='ORANGE'
-                lineStyle='DASHED'
-                lineWidth={1.5}
-              />
-              <Expression id='short' latex='g(x)\{0.02<x\}' color='GREEN' />
-              <Expression id='long' latex='f(x)\{0.02<x\}' color='PURPLE' />
-              <Expression
-                id='X'
-                latex='X=1'
-                sliderBounds={{ min: 0.02, max: '', step: '' }}
-              />
-              <Expression
-                id='p'
-                latex='p=\operatorname{round}\left(X\cdot2000\right)'
-                hidden
-              />
-              <Expression
-                id='Price'
-                latex='(X,-0.1)'
-                color='BLACK'
-                hidden
-                showLabel
-                label='$${p}'
-                labelOrientation='BELOW'
-              />
-              <Expression id='S' latex='(X,g(X))' color='GREEN' />
-              <Expression id='L' latex='(X,f(X))' color='PURPLE' />
-              <Expression
-                id='rB'
-                latex='x=X\{g(X)<y<3\}'
-                color='GREEN'
-                lineStyle='DASHED'
-                lineWidth={1.5}
-              />
-              <Expression
-                id='rA'
-                latex='x=X\{0<y<f(X)\}'
-                color='PURPLE'
-                lineStyle='DASHED'
-                lineWidth={1.5}
-              />
-              <Expression
-                id='lC'
-                latex='(X+0.15,(1.1g(X)+f(X))/2.1)'
-                color='ORANGE'
-                hidden
-                showLabel
-                label='LP'
-                labelOrientation='RIGHT'
-              />
-              <Expression
-                id='lB'
-                latex='(X-0.3,(g(X)+3)/2)'
-                color='GREEN'
-                hidden
-                showLabel
-                label='Short'
-                labelOrientation='LEFT'
-              />
-              <Expression
-                id='lA'
-                latex='(X+0.25,0.55f(X))'
-                color='PURPLE'
-                hidden
-                showLabel
-                label='Long'
-                labelOrientation='RIGHT'
-              />
-            </GraphingCalculator>
-          </div>
-        ) : (
-          <div className='flex flex-col justify-center items-center pb-[80px] pt-[80px] gap-6'>
-            <GraphingCalculator
-              attributes={{ className: 'calculator' }}
-              fontSize={14}
-              keypad
-              projectorMode={false}
-              settingsMenu={false}
-              expressionsCollapsed
-              expressions={false}
-              zoomButtons={false}
-              lockViewport={false}
-              invertedColors
-              border={false}
-              showGrid={false}
-              xAxisNumbers={false}
-              yAxisNumbers={false}
-              ref={calc}
-              xAxisArrowMode='POSITIVE'
-              yAxisArrowMode='POSITIVE'
-              xAxisLabel={TOKEN_R}
-              yAxisLabel='Value'
-            >
-              <Expression id='f' latex={FX} hidden />
-              <Expression id='g' latex={GX} hidden />
-              <Expression
-                id='lR'
-                latex={`(${X * 0.01},${R * 0.97})`}
-                color='RED'
-                hidden
-                showLabel
-                label='Pool Reserve'
-                labelOrientation={Desmos.LabelOrientations.RIGHT}
-              />
-              <Expression
-                id='R'
-                latex={`y=${R}\\{${PX}<x\\}`}
-                color='RED'
-                lineWidth={1.5}
-              />
-              <Expression
-                id='R1'
-                latex={`y=${R1}\\{${PX}<x\\}`}
-                color='RED'
-                lineWidth={1.5}
-                lineStyle='DASHED'
-              />
-              {/* <Expression
-                id='rC'
-                latex={`x=X\\{f(${P},X,${a},${R})<y<g(${P},X,${b},${R})\\}`}
-                color='ORANGE'
-                lineStyle='DASHED'
-                lineWidth={1.5}
-              /> */}
-              <Expression id='short' latex={`g(${P},x,${b},${R})\\{${PX}<x\\}`} color='GREEN' />
-              <Expression id='long' latex={`f(${P},x,${a},${R})\\{${PX}<x\\}`} color='PURPLE' />
-              <Expression id='short1' latex={`g(${P},x,${b},${R1})\\{${PX}<x\\}`} color='GREEN' lineStyle='DASHED' />
-              <Expression id='long1' latex={`f(${P},x,${a},${R1})\\{${PX}<x\\}`} color='PURPLE' lineStyle='DASHED' />
-              <Expression
-                id='X'
-                latex={`X=${X}`}
-                sliderBounds={{ min: PX, max: '', step: '' }}
-              />
-              <Expression
-                id='p'
-                latex={`p=\\operatorname{round}(X*${MARK})`}
-                hidden
-              />
-              <Expression
-                id='Price'
-                latex='(X,0)'
-                color='BLACK'
-                hidden
-                showLabel
-                label='$${p}'
-                labelOrientation={Desmos.LabelOrientations.BELOW}
-              />
-              <Expression id='S' latex={`(X,g(${P},X,${b},${R}))`} color='GREEN' />
-              <Expression id='L' latex={`(X,f(${P},X,${a},${R}))`} color='PURPLE' />
-              <Expression id='S1' latex={`(X,g(${P},X,${b},${R1}))`} color='GREEN' />
-              <Expression id='L1' latex={`(X,f(${P},X,${a},${R1}))`} color='PURPLE' />
-              {/* <Expression
-                id='rB'
-                latex={`x=X\\{g(${P},X,${b},${R})<y<${R}\\}`}
-                color='GREEN'
-                lineStyle='DASHED'
-                lineWidth={1.5}
-              />
-              <Expression
-                id='rA'
-                latex={`x=X\\{0<y<f(${P},X,${a},${R})\\}`}
-                color='PURPLE'
-                lineStyle='DASHED'
-                lineWidth={1.5}
-              /> */}
-              <Expression
-                id='dr'
-                latex={drLatex}
-                color={Desmos.Colors.BLACK}
-                lineWidth={3}
-              />
-              <Expression
-                id='lC'
-                latex={`(X,(1.1g(${P},X,${b},${R})+f(${P},X,${a},${R}))/2.1)`}
-                color={Desmos.Colors.BLACK}
-                hidden
-                showLabel
-                label='LP'
-                labelOrientation={Desmos.LabelOrientations.RIGHT}
-              />
-              <Expression
-                id='lB'
-                latex={`(X*0.9,(g(${P},X,${b},${R})+${R})/2)`}
-                color={Desmos.Colors.GREEN}
-                hidden
-                showLabel
-                label='Short'
-                labelOrientation={Desmos.LabelOrientations.LEFT}
-              />
-              <Expression
-                id='lA'
-                latex={`(X*1.1,0.55f(${P},X,${a},${R}))`}
-                color={Desmos.Colors.PURPLE}
-                hidden
-                showLabel
-                label='Long'
-                labelOrientation={Desmos.LabelOrientations.RIGHT}
-              />
-            </GraphingCalculator>
-          </div>
-        )}
+        <div className='flex flex-col justify-center items-center pb-[80px] pt-[80px] gap-6'>
+          <GraphingCalculator
+            attributes={{ className: 'calculator' }}
+            fontSize={14}
+            keypad
+            projectorMode={false}
+            settingsMenu={false}
+            expressionsCollapsed
+            expressions={false}
+            zoomButtons={false}
+            lockViewport={false}
+            invertedColors
+            border={false}
+            showGrid={false}
+            xAxisNumbers={false}
+            yAxisNumbers={false}
+            ref={calc}
+            xAxisArrowMode='POSITIVE'
+            yAxisArrowMode='POSITIVE'
+            xAxisLabel={TOKEN_R}
+            yAxisLabel='Value'
+          >
+            <Expression id='f' latex={FX} hidden />
+            <Expression id='g' latex={GX} hidden />
+            <Expression
+              id='lR'
+              latex={`(${X * 0.01},${R * 0.97})`}
+              color='RED'
+              hidden
+              showLabel
+              label='Pool Reserve'
+              labelOrientation={Desmos.LabelOrientations.RIGHT}
+            />
+            <Expression
+              id='R'
+              latex={`y=${R}\\{${PX}<x\\}`}
+              color='RED'
+              lineWidth={1.5}
+            />
+            <Expression
+              id='R1'
+              latex={`y=${R1}\\{${PX}<x\\}`}
+              color='RED'
+              lineWidth={1.5}
+              lineStyle='DASHED'
+            />
+            {/* <Expression
+              id='rC'
+              latex={`x=X\\{f(${P},X,${a},${R})<y<g(${P},X,${b},${R})\\}`}
+              color='ORANGE'
+              lineStyle='DASHED'
+              lineWidth={1.5}
+            /> */}
+            <Expression id='short' latex={`g(${P},x,${b},${R})\\{${PX}<x\\}`} color='GREEN' />
+            <Expression id='long' latex={`f(${P},x,${a},${R})\\{${PX}<x\\}`} color='PURPLE' />
+            <Expression id='short1' latex={`g(${P},x,${b},${R1})\\{${PX}<x\\}`} color='GREEN' lineStyle='DASHED' />
+            <Expression id='long1' latex={`f(${P},x,${a},${R1})\\{${PX}<x\\}`} color='PURPLE' lineStyle='DASHED' />
+            <Expression
+              id='X'
+              latex={`X=${X}`}
+              sliderBounds={{ min: PX, max: '', step: '' }}
+            />
+            <Expression
+              id='p'
+              latex={`p=\\operatorname{round}(X*${MARK})`}
+              hidden
+            />
+            <Expression
+              id='Price'
+              latex='(X,0)'
+              color='BLACK'
+              hidden
+              showLabel
+              label='$${p}'
+              labelOrientation={Desmos.LabelOrientations.BELOW}
+            />
+            <Expression id='S' latex={`(X,g(${P},X,${b},${R}))`} color='GREEN' />
+            <Expression id='L' latex={`(X,f(${P},X,${a},${R}))`} color='PURPLE' />
+            <Expression id='S1' latex={`(X,g(${P},X,${b},${R1}))`} color='GREEN' />
+            <Expression id='L1' latex={`(X,f(${P},X,${a},${R1}))`} color='PURPLE' />
+            {/* <Expression
+              id='rB'
+              latex={`x=X\\{g(${P},X,${b},${R})<y<${R}\\}`}
+              color='GREEN'
+              lineStyle='DASHED'
+              lineWidth={1.5}
+            />
+            <Expression
+              id='rA'
+              latex={`x=X\\{0<y<f(${P},X,${a},${R})\\}`}
+              color='PURPLE'
+              lineStyle='DASHED'
+              lineWidth={1.5}
+            /> */}
+            <Expression
+              id='dr'
+              latex={drLatex}
+              color={Desmos.Colors.BLACK}
+              lineWidth={3}
+            />
+            <Expression
+              id='lC'
+              latex={`(X,(1.1g(${P},X,${b},${R})+f(${P},X,${a},${R}))/2.1)`}
+              color={Desmos.Colors.BLACK}
+              hidden
+              showLabel
+              label='LP'
+              labelOrientation={Desmos.LabelOrientations.RIGHT}
+            />
+            <Expression
+              id='lB'
+              latex={`(X*0.9,(g(${P},X,${b},${R})+${R})/2)`}
+              color={Desmos.Colors.GREEN}
+              hidden
+              showLabel
+              label='Short'
+              labelOrientation={Desmos.LabelOrientations.LEFT}
+            />
+            <Expression
+              id='lA'
+              latex={`(X*1.1,0.55f(${P},X,${a},${R}))`}
+              color={Desmos.Colors.PURPLE}
+              hidden
+              showLabel
+              label='Long'
+              labelOrientation={Desmos.LabelOrientations.RIGHT}
+            />
+          </GraphingCalculator>
+        </div>
       </Card>
     </React.Fragment>
   )
