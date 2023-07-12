577f20e55c874517f1a2f792286f2514dc42255d
Zergity <zergity@gmail.com>
2023-07-12 22:15:37 +0700

    update Settings texts

--- src/Components/SettingModal/index.tsx
+++ src/Components/SettingModal/index.tsx
@@ -31,7 +31,7 @@ const Component = ({
         className='swap-setting-modal'
       >
         <div className='info-row'>
-          <Text>Slippage</Text>
+          <Text>Slippage Tolerance</Text>
           <Input
             value={settings.slippage}
             onChange={(e) => {
@@ -41,7 +41,7 @@ const Component = ({
           />
         </div>
         <div className='info-row'>
-          <Text>Pay-Off Min Rate</Text>
+          <Text>Payoff Tolerance</Text>
           <Input
             value={settings.payoffMinRate}
             onChange={(e) => {
