ce30c34f8e0f6b51ad432ca13eb32461348243e1
Zergity <zergity@gmail.com>
2023-08-07 17:19:38 +0700

    re add the minimum opacity

--- src/hooks/useGenerateLeverageData.ts
+++ src/hooks/useGenerateLeverageData.ts
@@ -36,12 +36,11 @@ export const useGenerateLeverageData = (tradeType: TRADE_TYPE) => {
           return
         }
 
-        // const opacity = 1 - 0.95 * deleverageRisk
         // const color = 'rgb(0, 180, 255)'
         // const bgColor = 'rgb(27, 29, 33)'
         // const color = opacity === 0 ? bgColor : `rgb(0, ${opacity * 180}, ${opacity * 255})`
-        const opacity = 1 - Math.sqrt(deleverageRisk)
-        const color = `rgb(${0 + (1 - opacity) * 27}, ${opacity * 180 + (1 - opacity) * 29}, ${opacity * 255 + (1 - opacity) * 33})`
+        const transparency = 1 - 0.95 * deleverageRisk
+        const color = `rgb(${0 + (1 - transparency) * 27}, ${transparency * 180 + (1 - transparency) * 29}, ${transparency * 255 + (1 - transparency) * 33})`
         const power = Math.abs(Number(getTokenPower(pool.TOKEN_R, pool.baseToken, tradeTypeToId(tradeType), pool.k.toNumber())))
         const size = pool.states.R
 
@@ -69,7 +68,7 @@ export const useGenerateLeverageData = (tradeType: TRADE_TYPE) => {
             token: pool.poolAddress + '-' + tradeTypeToId(tradeType),
             size,
             color,
-            opacity,
+            opacity: transparency,
             dailyInterestRate: pool.dailyInterestRate,
             deleverageRisk
           })
