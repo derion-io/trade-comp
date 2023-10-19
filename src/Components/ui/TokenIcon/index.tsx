fbad65764346f61abe9c0b07feb164e8e5bd800b
athevinha <nguyenthevinh1002@gmail.com>
2023-10-19 14:34:28 +0700

    override token fontsize

--- src/Components/ui/TokenIcon/index.tsx
+++ src/Components/ui/TokenIcon/index.tsx
@@ -108,6 +108,6 @@ export const TokenIcon = (props: {
         }}
         {...props}
         src={configs.tokens?.[props?.tokenAddress || '']?.logo || src}
-      /> : <span>{configs.tokens?.[props?.tokenAddress || '']?.logo}</span>
+      /> : <span style={{ fontSize: '1em' }}>{configs.tokens?.[props?.tokenAddress || '']?.logo}</span>
   }
 }
