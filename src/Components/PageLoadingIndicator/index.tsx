44a0037b76178b8860cd2c971bb5062a1aaeee41
athevinha <nguyenthevinh1002@gmail.com>
2024-03-26 12:52:34 +0700

    Rebrand logo derion

--- src/Components/PageLoadingIndicator/index.tsx
+++ src/Components/PageLoadingIndicator/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './style.scss'
-import { DerivableIcon } from '../ui/Icon'
+import { DerionIcon } from '../ui/Icon'
 
 export const PageLoadingIndicator = () => {
   return (
     <div className='page-loading-indicator__wrap'>
       <div className='page-loading-indicator__logo'>
-        <DerivableIcon width={360} height={60} />
+        <DerionIcon width={360} height={60} />
       </div>
       <div className='page-loading-indicator'>
         <div />
