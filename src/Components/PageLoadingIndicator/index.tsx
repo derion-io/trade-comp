17c1db93b5ba94fd5e65de7b149b521a19068f38
Zergity <zergity@gmail.com>
2024-04-07 09:38:26 +0700

    update new logo

--- src/Components/PageLoadingIndicator/index.tsx
+++ src/Components/PageLoadingIndicator/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './style.scss'
-import { DerionIcon } from '../ui/Icon'
+import { Logo } from '../ui/Icon'
 
 export const PageLoadingIndicator = () => {
   return (
     <div className='page-loading-indicator__wrap'>
       <div className='page-loading-indicator__logo'>
-        <DerionIcon width={360} height={60} />
+        <Logo width={360} height={60} />
       </div>
       <div className='page-loading-indicator'>
         <div />
