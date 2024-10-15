1b1a3469efb18b83f5a85881a9102861c231655a
Zergity <zergity@gmail.com>
2024-10-15 14:06:40 +0700

    spinner size

--- src/Components/BuyPositionBox/components/PositionLoading.tsx
+++ src/Components/BuyPositionBox/components/PositionLoading.tsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import {SvgSpinners12DotsScaleRotate} from '../../ui/Icon'
 export const PositionLoadingComponents = () => {
-  // if (isPhone) {
-  //   return <Fragment>
-  //   {Array.from({ length: POSITION_LOADING_COUNT }).map((id, _) => {
-  //     return (
-  //       <InfoRow key={_}>
-  //             <SkeletonLoader loading={true} style={{ width: '100%', marginBottom:'1rem'}} height='50px'/>
-  //       </InfoRow>
-  //     )
-  //   })}
-  // </Fragment>
-  // } else {
-    return (
-      <div style={{ 
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        width: '100%', 
-        minHeight: '160px'  // min Height of Position & History tabs
-      }}>
-        <SvgSpinners12DotsScaleRotate width={'50px'} height={'50px'}/>
-      </div>
-    )
-  // }
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      width: '100%', 
+      minHeight: '50px'  // min Height of Position & History tabs
+    }}>
+      <SvgSpinners12DotsScaleRotate width={'38px'} height={'38px'}/>
+    </div>
+  )
 }
