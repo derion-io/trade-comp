d1492f2d0f66102fe52becc8a4113f275a52962b
athevinha <nguyenthevinh1002@gmail.com>
2024-10-15 11:40:59 +0700

    cleanup

--- src/Components/BuyPositionBox/components/PositionLoading.tsx
+++ src/Components/BuyPositionBox/components/PositionLoading.tsx
@@ -1,22 +1,6 @@
-import React, {Fragment} from 'react'
-import { useWindowSize } from '../../../hooks/useWindowSize'
-import { SkeletonLoader } from '../../ui/SkeletonLoader'
-import { useCurrentPoolGroup } from '../../../state/currentPool/hooks/useCurrentPoolGroup'
-import { TRADE_TYPE } from '../../../utils/constant'
-import {InfoRow} from '../../ui/InfoRow'
-import LineChartLoaderSVG from '../../ChartLoaders/LineChartLoaderSVG'
+import React from 'react'
 import {SvgSpinners12DotsScaleRotate} from '../../ui/Icon'
-import {Card} from 'antd'
-import {Box} from '../../ui/Box'
-const POSITION_LOADING_COUNT = 3
 export const PositionLoadingComponents = () => {
-  const { width } = useWindowSize()
-  const isPhone = width && width < 992
-  const { tradeType } = useCurrentPoolGroup()
-
-  const showSize = tradeType !== TRADE_TYPE.LIQUIDITY
-  const skeletonCount = showSize ? 6 : 5
-
   // if (isPhone) {
   //   return <Fragment>
   //   {Array.from({ length: POSITION_LOADING_COUNT }).map((id, _) => {
