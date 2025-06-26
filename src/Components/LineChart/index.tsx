84e9d2d2c75fd714d2a58342505a32697178e744
athevinha <nguyenthevinh1002@gmail.com>
2025-06-26 15:50:44 +0700

    linechart timeframe

--- src/Components/LineChart/index.tsx
+++ src/Components/LineChart/index.tsx
@@ -290,7 +290,7 @@ const Component = ({ changedIn24h }: { changedIn24h: number }) => {
                 axisLine={false}
                 tickLine={false}
                 tickFormatter={(time) =>
-                  moment(time).format(interval === I_1D ? 'HH:mm' : 'DD/MM')
+                  moment(time).format(interval === I_1D ? 'DD/MM' : 'HH:mm')
                 }
                 minTickGap={60}
                 interval="preserveEnd"
